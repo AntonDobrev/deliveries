c8ba0a3e4bf125e1b9a308512ce7d1435efca1f1
Anton <ant.dobrev@gmail.com>
2017-02-23 18:53:06 +0200

    Edits

--- NativeScriptViewsAngular/app/modules/homeView/shared/templates/homeView-edit.component.ts
+++ NativeScriptViewsAngular/app/modules/homeView/shared/templates/homeView-edit.component.ts
@@ -3,17 +3,8 @@ import { NotificationService } from "../../../../shared/services"
 
 import * as common from "./";
 import * as shared from "../../../../shared";
+import { OrderStatus } from "../../../../shared/enums";
 
-// TODO - place in a separate file
-enum OrderStatus {
-    Pending = 1,
-    Current = 2,
-    Delivered = 3,
-    Refused = 4,
-    Problem = 10
-}
-
-// TODO - set as a constant in the dedicated file
 const orderStatusNames = {
     "Pending": OrderStatus.Pending,
     "Current": OrderStatus.Current,
@@ -22,6 +13,7 @@ const orderStatusNames = {
     "Problem": OrderStatus.Problem
 };
 
+
 @Component({
     moduleId: module.id,
     selector: "ns-homeView-edit",
@@ -29,19 +21,12 @@ const orderStatusNames = {
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeViewEditComponent {
-
-    private statusArray = [OrderStatus.Pending, OrderStatus.Current, OrderStatus.Delivered, OrderStatus.Refused, OrderStatus.Problem];
-
-    private directions = ["Pending", "Current", "Delivered", "Refused", "Problem"];
+    
+    statusNamesArray:string[] = ["Pending", "Current", "Delivered", "Refused", "Problem"];
 
     @Input() set current(value: shared.Item) {
         this.item = (<any>Object).assign({}, value);
-        // TODO - this works, refactor
-        console.log("Status[this.item.data.Status]" + OrderStatus[this.item.data.Status]); // Current, etc.
-
-        console.log("orderStatusNames[this.item.data.Status]" + orderStatusNames[OrderStatus[this.item.data.Status]]); // 1, 2, 10 etc.
-        this.currentItemStatus = this.directions.indexOf(OrderStatus[this.item.data.Status]);
-        console.log("this.currentItemStatus: " + this.currentItemStatus); 1
+        this.currentItemStatusIndex = this.statusNamesArray.indexOf(OrderStatus[this.item.data.Status]); // indexOf("Pending")
     }
 
     @Output() update = new EventEmitter();
@@ -53,27 +38,21 @@ export class HomeViewEditComponent {
 
     }
 
-    currentItemStatus: number; // = this.item.data.Status; //this.directions.indexOf(this.directions[this.item.data.Status].toString()); // TODO - use this in the details view too
+    currentItemStatusIndex: number;
 
     selectedIndexChanged(picker) {
-        console.log('picker selection: ' + picker.selectedIndex);
-        var selectedItem: string = this.directions[picker.selectedIndex].toString();
-        console.log(selectedItem);
-
-        var enumIndex = OrderStatus[selectedItem];
-
-        console.log("Enum index: " + enumIndex); // TODO - works correctly
-        this.item.data.Status = enumIndex;
+        var selectedStatusName: string = this.statusNamesArray[picker.selectedIndex].toString();
+        var statusIndex = OrderStatus[selectedStatusName]; // 1, 2, , 10 
+        this.item.data.Status = statusIndex;
 
     }
     onUpdate() {
-        console.log(JSON.stringify(this.item));
         this.update.emit({
             item: this.item
         });
     }
 
-    onDelete() {
+    onDelete() {l
         this._notificationService.confirm("Deleting an item").then(isConfirmed => {
             if (isConfirmed) {
                 this.delete.emit({
