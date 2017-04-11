dd2e102f1941122976f624ef3acd42f528c1afb5
Anton <ant.dobrev@gmail.com>
2017-04-11 15:14:43 +0300

    yet another validation

--- NativeScriptViewsAngular/app/modules/homeView/shared/templates/homeView-add.component.ts
+++ NativeScriptViewsAngular/app/modules/homeView/shared/templates/homeView-add.component.ts
@@ -1,8 +1,10 @@
 import { Delivery } from './../../../../shared/models/delivery.model';
-import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter, AfterViewChecked } from "@angular/core";
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { TextField } from "ui/text-field";
 import { OrderStatus } from "../../../../shared";
+
+import { NotificationService } from "../../../../shared/services"
 
 @Component({
     moduleId: module.id,
@@ -10,56 +12,34 @@ import { OrderStatus } from "../../../../shared";
     templateUrl: "homeView-add.component.html",
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeViewAddComponent implements AfterViewChecked {
-    // @Input() provider: any;
-    //  @Input() item: Delivery;
-    @Input() isValid: Boolean = false;
+export class HomeViewAddComponent {
 
     @Input() set current(value: Delivery) {
         this.item = new Delivery;
         this.item.Status = OrderStatus.Pending;
     }
 
-    ngOnChanges() {
-        
-    }
-  ngAfterViewChecked() {
-      
-      // TODO - check form validity here and set the isValid flag // perhaps with the min length property
-   
-
-    
-}
-
-// iterrate over properties and check them 
-// isDeliveryItemValid(item:Delivery) {
-//     return item.DeliveryName && item.DeliveryItem && item.DeliveryItemType && item.DeliveryAddressCity && item.DeliveryAddressLine1 && item.DeliveryAddressPostcode; 
-// }
-    // @ViewChild('deliveryItemElement') itemElement: TextField;
-
     @Output() add = new EventEmitter();
 
     @Input() item: Delivery;
 
-    constructor() { }
+    constructor(private _notificationService: NotificationService) { }
 
     onSave() {
-              var item = this.item;
+        var item = this.item;
 
-  if(item.DeliveryName && item.DeliveryItem && item.DeliveryItemType && item.DeliveryAddressCity && item.DeliveryAddressLine1 && item.DeliveryAddressPostcode && item.DeliveryName.length > 0) {
-        this.isValid = true;
-          this.add.emit({
-            item: this.item
-        });
-      } else {
-          // TODO - handle the case to hide the button when a valid form is deactivated
-          this.isValid = false;
-          return;
-      }
-      console.log("isValid: " + this.isValid + 'name ' + this.item.DeliveryName);
-
-      
+        if (this.validateDeliveryItem(item)) {
+            this.add.emit({
+                item: this.item
+            })
+        } else {
+            this._notificationService.warning("Please fill all required fields");
+            return;
+        }
     }
 
-
+    validateDeliveryItem(item: Delivery) {
+        var isDeliveryItemValid = (item.DeliveryName && item.DeliveryItem && item.DeliveryItemType && item.DeliveryAddressCity && item.DeliveryAddressLine1 && item.DeliveryAddressPostcode);
+        return isDeliveryItemValid;
+    }
 }
